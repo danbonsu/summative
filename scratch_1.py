b323983509a6e5182fe14ca312446b14c723a104
github_danbonsu <dbonsu18@aiip.alueducation.com>
2018-02-20 14:57:49 +0000

    Began problem 3 thought process and addressed two of the acceptance criteria

--- scratch_1.py
+++ scratch_1.py
@@ -1,6 +1,8 @@
+#Libraries (Random, Time)
 import random as rand
 from datetime import datetime
 
+#Problem 1 (32 Sensors taking 16 floats each)
 sensorCluster = []
 readings = []
 
@@ -8,15 +10,19 @@ for i in range(1,33):
     for n in range(1,17):
         sensorDataNumber = rand.random()
         readings.append(sensorDataNumber)
-#appends to array
+#appends from readings array to sensor cluster array
     sensorCluster.append(readings)
+#empty readings array
     readings=[]
 
 
-#creates timestamp and writes data to file
+#Problem 2 (creates timestamp and writes data to file)
 with open("test.txt", "a") as f:
      st_time = datetime.now().strftime('%Y%m%d_%H:%M:%S - ')
-     f.write(st_time)
-     f.write("{}".format(sensorCluster))
      f.write('\n')
+     f.write(st_time)
+     f.write('\n' + "{}".format(sensorCluster))
+     #f.write('\n')
+
+
 
