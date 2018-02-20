19f147f4a8da3d875273f81388493baa74919e09
github_danbonsu <dbonsu18@aiip.alueducation.com>
2018-02-20 18:37:36 +0000

    Final changes implemented, folder cleaned up

--- pythonsummative_Daniel.py
+++ pythonsummative_Daniel.py
@@ -1,19 +1,17 @@
-#Problem3
-
-#Libraries (Random, Time)
+#Imported Libraries (Random, Time)
 import random as rand
 from datetime import datetime
 
-#Problem 1 (32 Sensors taking 16 floats each)
+#Created sensorCluster array to hold 32 sensors and readings array to hold 16 floats
 sensorCluster = []
 readings = []
 
-
+#Created function which checks for error. Takes param as array number and returns err
 def checkerror(x):
         if 'err' in sensorCluster[x]:
             return 'err'
 
-
+#Loop which begins populating sensor clusters with data
 for i in range(1,33):
     for n in range(1,17):
         sensorDataNumber = rand.random()
@@ -21,25 +19,22 @@ for i in range(1,33):
 
 #appends from readings array to sensor cluster array
     sensorCluster.append(readings)
-#empty readings array
+#empty readings array and begins loop with new set of 16 floats
     readings=[]
 
+#Created corrupted data at the following indexes
 sensorCluster[3].append("err")
 sensorCluster[4].append("err")
 sensorCluster[6].append("err")
 
-#print sensorCluster[31]
-
-#Problem 2 (creates timestamp and writes data to file)
+#Creates timestamp and writes sensor data to file
 with open("test.txt", "a") as f:
      st_time = datetime.now().strftime('%Y%m%d_%H:%M:%S - ')
      f.write('\n')
      f.write(st_time)
      f.write('\n' + "{}".format(sensorCluster))
-     #f.write('\n')
 
-
-#Problem 3
+#Check sensorCluster array and calls check error function, if there is error write to error log file
 for counter, value in enumerate(sensorCluster):
  if checkerror(counter):
     #print (counter,value)
@@ -48,6 +43,6 @@ for counter, value in enumerate(sensorCluster):
         st_time = datetime.now().strftime('%Y%m%d_%H:%M:%S - ')
         f.write('\n')
         f.write(st_time)
-        f.write("Problem happened with sensor at location" + '%d' % counter)
+        f.write("Problem happened with sensor at location " + '%d' % counter)
 
 
