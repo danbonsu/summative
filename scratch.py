0a89b339ec0146147c187ea5c2a9d2f08fb5035b
github_danbonsu <dbonsu18@aiip.alueducation.com>
2018-02-20 18:09:42 +0000

    Created complete solution for problem three

--- scratch.py
+++ scratch.py
@@ -4,14 +4,15 @@
 import random as rand
 from datetime import datetime
 
-def checkError():
-    if 'err' in sensorCluster:
-
-        print 'error'
-
 #Problem 1 (32 Sensors taking 16 floats each)
 sensorCluster = []
 readings = []
+
+
+def checkerror(x):
+        if 'err' in sensorCluster[x]:
+            return 'err'
+
 
 for i in range(1,33):
     for n in range(1,17):
@@ -20,12 +21,14 @@ for i in range(1,33):
 
 #appends from readings array to sensor cluster array
     sensorCluster.append(readings)
-
 #empty readings array
     readings=[]
 
-sensorCluster.append("err")
-checkError()
+sensorCluster[3].append("err")
+sensorCluster[4].append("err")
+sensorCluster[6].append("err")
+
+#print sensorCluster[31]
 
 #Problem 2 (creates timestamp and writes data to file)
 with open("test.txt", "a") as f:
@@ -35,5 +38,16 @@ with open("test.txt", "a") as f:
      f.write('\n' + "{}".format(sensorCluster))
      #f.write('\n')
 
+
+#Problem 3
+for counter, value in enumerate(sensorCluster):
+ if checkerror(counter):
+    #print (counter,value)
+
+    with open("log.txt", "a") as f:
+        st_time = datetime.now().strftime('%Y%m%d_%H:%M:%S - ')
+        f.write('\n')
+        f.write(st_time)
+        f.write("Problem happened with sensor at location" + '%d' % counter)
 
 
